b12187017ec6a2f5ca6e585fe223131377aa4bb2
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2024-12-22 00:23:08 +0530

    stating 8 map problems are completed

---
+++ mapSoution.js
@@ -0,0 +1,72 @@
+// helperFunction+++++++++++++++++++++++++++++
+const evaluateTruthiness = function (data) {
+  if (data) {
+    return true;
+  }
+
+  return false;
+}
+
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
+}
+
+const createDublicate = function (string) {
+  return string + string;
+}
+//problems+++++++++++++++++++++++++++++++++++++
+
+// squares of [1, 2, 3] => [1, 4, 9]
+const squaresOf = function (numbers) {
+  return numbers.map(function (number) {
+    return Math.pow(number, 2);
+  });
+};
+
+// lengths of ["apple", "banana", "kiwi"] => [5, 6, 4]
+const lengthsOf = function (strings) {
+  return strings.map(function (string) {
+    return string.length;
+  });
+};
+
+// uppercase of ["hello", "world"] => ["HELLO", "WORLD"]
+const uppercaseOf = function (strings) {
+  return strings.map(function (string) {
+    return string.toUpperCase();
+  });
+};
+
+// first characters of ["apple", "banana", "kiwi"] => ["a", "b", "k"]
+const firstCharactersOf = function (strings) {
+  return strings.map(function (string) {
+    return string.at(0);
+  });
+};
+
+// truth values of [0, 1, 2, 3] => [false, true, true, true]
+// Assume non-zero numbers are true, and zero is false
+const truthValuesOf = function (numbers) {
+  return numbers.map(function (number) {
+    return evaluateTruthiness(number);
+  });
+};
+
+// reverse strings of ["hello", "world"] => ["olleh", "dlrow"]
+const reversedStringsOf = function (strings) {
+  return strings.map(function (string) {
+    return reverseString(string);
+  });
+};
+
+const doubleCharsOfString = function (string) {
+  return string.split("").reduce(function (accumolator, char) {return accumolator + createDublicate(char)},"" )
+};
+
+// double letters of ["cat", "dog", "bat"] => ["ccaat", "ddoog", "bbaatt"]
+const doubleLettersOf = function (strings) {
+  return strings.map(doubleCharsOfString);
+};
+
+// boolean negation of [true, false, true] => [false, true, false]
+const negatedBooleansOf = function (booleans) { };
