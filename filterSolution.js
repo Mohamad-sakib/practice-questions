c6d8ac005bc418e882fe343fa6c37045ffe2c145
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2024-12-21 16:58:22 +0530

    filter 1st and 2nd problem solved

--- filterSolution.js
+++ filterSolution.js
@@ -1,5 +1,20 @@
-const isEven = function (number) {return number % 2 === 0}
+const isEven = function (number) {return number % 2 === 0};
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
 const filterEvenNumbers = function (numbers) { 
   return numbers.filter(isEven);
+};  
+
+// words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
+const isMoreThen = function (val1) {
+  return function (val2) {
+    return val2 > val1;
+  }
+}
+
+const isLengthMoreThen5 = isMoreThen(5);
+
+const filterLongWords = function (words) { 
+  return words.filter(function (word) {
+    return isLengthMoreThen5(word.length);
+  });  
 };
