80fd13e2409043232510208dedc95539b1ad6f93
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2024-12-21 20:00:20 +0530

    problem 8, filter all student of students having grade more then 80 same pattern is done using morethen()

--- filterSolution.js
+++ filterSolution.js
@@ -7,7 +7,7 @@ const isMoreThen = function (val1) {
   }
 };
 
-const checkStatusFor = function (activityStatus) {return activityStatus};
+const checkStatusFor = function (activityStatus) { return activityStatus };
 
 // problems++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -33,7 +33,7 @@ const filterAdults = function (people) {
 };
 
 // active users [{username: "alice", active: true}, {username: "bob", active: false}] => [{username: "alice", active: true}]
-const filterActiveUsers = function (users) { 
+const filterActiveUsers = function (users) {
   return users.filter(function (user) {
     return checkStatusFor(user.active);
   });
@@ -42,22 +42,31 @@ const filterActiveUsers = function (users) {
 const isGreaterThen10 = isMoreThen(10);
 
 // numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
-const filterNumbersGreaterThanTen = function (numbers) { 
+const filterNumbersGreaterThanTen = function (numbers) {
   return numbers.filter(isGreaterThen10);
 };
 
 const arePagesMoreThen200 = isMoreThen(200);
 
 // books with more than 200 pages [{title: "Book 1", pages: 150}, {title: "Book 2", pages: 250}] => [{title: "Book 2", pages: 250}]
-const filterLongBooks = function (books) { 
+const filterLongBooks = function (books) {
   return books.filter(function (book) {
     return arePagesMoreThen200(book.pages);
   });
 };
 
 // users with incomplete profiles [{username: "alice", profileComplete: true}, {username: "bob", profileComplete: false}] => [{username: "bob", profileComplete: false}]
-const filterIncompleteProfiles = function (users) { 
+const filterIncompleteProfiles = function (users) {
   return users.filter(function (user) {
     return checkStatusFor(user.profileComplete);
+  });
+};
+
+// students with grades above 80 [{name: "John", grade: 75}, {name: "Jane", grade: 85}] => [{name: "Jane", grade: 85}]
+const isGradeMorethen80 = isMoreThen(80);
+
+const filterHighGrades = function (students) {
+  return students.filter(function (student) {
+    return isGradeMorethen80(student.grade);
   });
 };
