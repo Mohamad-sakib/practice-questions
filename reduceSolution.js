f513f9d97bb73e21bc5e498877937435ef0efa74
Mohamad-sakib <158846349+Mohamad-sakib@users.noreply.github.com>
2024-12-22 12:04:59 +0530

    starting 6 problems are done of reduce

--- reduceSolution.js
+++ reduceSolution.js
@@ -2,16 +2,57 @@
 
 const sumOfTwoNumber = function (num1, num2) {
   return num1 + num2;
-}; 
+};
 
+const productOfTwoNumber = function (num1, num2) {
+  return num1 * num2;
+};
 
+const minOfTwoNumber = function (num1, num2) {
+  return Math.min(num1, num2);
+};
 
+const maxOfTwoNumber = function (num1, num2) {
+  return Math.max(num1, num2);
+};
 
-
+const isPositive = function (number) {
+  return number > 0;
+}
 
 // problems--------------------------------------------------------------
 
 // sumOf([1, 2, 3, 4]) => 10
-const sumOf = function (numbers) { 
-  return numbers.reduce(sumOfTwoNumber,0);
+const sumOf = function (numbers) {
+  return numbers.reduce(sumOfTwoNumber, 0);
+}
+
+// productOf([1, 2, 3, 4]) => 24
+const productOf = function (numbers) {
+  if (numbers.length === 0) {
+    return "noValue To compute";
+  }
+
+  return numbers.reduce(productOfTwoNumber, 1);
+}
+
+// averageOf([1, 2, 3, 4, 5]) => 3
+const averageOf = function (numbers) {
+  return sumOf(numbers) / numbers.length;
+};
+
+
+// minOf([3, 1, 4, 1, 5, 9, 2]) => 1
+const minOf = function (numbers) {
+  return numbers.reduce(minOfTwoNumber, Infinity);
+};
+
+// maxOf([3, 1, 4, 1, 5, 9, 2]) => 9
+const maxOf = function (numbers) {
+  return numbers.reduce(maxOfTwoNumber, -Infinity)
+};
+
+// sumPositiveNumbers([1, -2, 3, -4]) => 4
+const sumPositiveNumbers = function (numbers) {
+  return sumOf(numbers.filter(isPositive));
 }
